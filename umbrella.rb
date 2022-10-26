0e8f8cd58e781a1f1fddad734ebd035c4952f73c
gsista-booth <gsista@chicagobooth.edu>
2022-10-26 02:46:07 +0000

    Gmap and darksky working

--- umbrella.rb
+++ umbrella.rb
@@ -8,7 +8,9 @@ p user_location
 
 require "open-uri"
 
-gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U"
+gmap_key = ENV.fetch("GMAPS_KEY")
+
+gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmap_key}"
 
 # using_gsub = "https://maps.googleapis.com/maps/api/geocode/json?address=PLACEHOLDER&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U".gsub("PLACEHOLDER", user_location)
 
@@ -39,3 +41,14 @@ p longitude
 # Use latitude and longitude to compose the correct
 #   endpoint in Dark Sky's API
 # Print the current temperature
+
+dark_sky_key = ENV.fetch("DARK_SKY_KEY")
+
+dark_sky_net_url = "https://api.darksky.net/forecast/#{dark_sky_key}"
+
+p dark_sky_key
+p dark_sky_net_url
+
+dark_sky_net_url = dark_sky_net_url + "/" + latitude.to_s + "," + longitude.to_s
+
+p dark_sky_net_url
